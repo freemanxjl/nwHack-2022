e03562dea8c4072c52adfabac8603e529373cbef
Gordon Lim <63278638+gordonlim02@users.noreply.github.com>
2023-01-21 16:50:16 -0800

    Combined imageRender.js into Results.js

--- pages/Results.js
+++ pages/Results.js
@@ -11,6 +11,7 @@ export function Results() {
     const [input, setInput] = useState("");
     const [recipe, setRecipe] = useState("");
     const [nutrition, setNutrition] = useState("");
+    const [image, setImage] = useState("");
 
     useEffect(() => {
     }, []);
@@ -51,6 +52,22 @@ export function Results() {
         }).catch(function (error) {
             console.error(error);
         });
+
+        const image = {
+            method: 'GET',
+            url: 'https://bing-image-search1.p.rapidapi.com/images/search',
+            params: {q: input},
+            headers: {
+              'X-RapidAPI-Key': '18a4b0517dmsh287b579413b5a7ep1645dbjsn3b7785018934',
+              'X-RapidAPI-Host': 'bing-image-search1.p.rapidapi.com'
+            }
+          };
+          
+          axios.request(image).then(function (response) {
+              setImage(response.data.value[0].contentUrl)
+          }).catch(function (error) {
+              console.error(error);
+          });
     }
 
     return (
@@ -84,6 +101,7 @@ export function Results() {
             {"Cholesterol (mg) : " + nutrition[0].cholesterol_mg}
             <br></br>
             </StyledDiv>}
+        {image && <img src={image} width="200px" height="200px"/>}
     </div>
   );
 
