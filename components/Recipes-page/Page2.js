68249c8b4d27c0be4208a14c8355d5d04b2c85d0
Freeman L <freemanxjl@gmail.com>
2023-01-22 11:51:52 -0800

    Update Page2.js

--- components/Recipes-page/Page2.js
+++ components/Recipes-page/Page2.js
@@ -4,7 +4,32 @@ import React  from 'react';
 import {useState, useEffect} from 'react'
 import styled from 'styled-components';
 import Page3 from './Page3';
+import setterRecipes from './setterRecipes';
 
+
+const SearchBar = styled.input`
+    height:3rem;
+    border:none;
+    width: 100% !important;
+    max-width: 40rem;
+    font-size:1.1rem;
+    font-weight: 300;
+    outline: none;
+    margin-left: 1rem;
+    margin-right: 1rem;
+    border-radius:30px;
+    border:1px solid #dcdcdc;
+    padding-left: 3rem;
+
+    &:hover {
+        box-shadow: 1px 1px 8px 1px #dcdcdc;
+    }
+
+    &:focus-within{
+        box-shadow: 1px 1px 8px 1px #dcdcdc;
+        outline:none;
+    }
+`;
 
 const RecipeList = styled(List)`
 `;
@@ -14,37 +39,39 @@ export function Page2({input, setInput, recipe, setRecipe, chosenRecipe, setChos
     }, []);
 
       
-    const handleSubmit = (event) => {
-        event.preventDefault();  
+    // const handleSubmit = (event) => {
+    //     event.preventDefault();  
 
-        const axios = require("axios");
+    //     const axios = require("axios");
 
-        const recipe = {
-            method: 'GET',
-            url: 'https://recipe-by-api-ninjas.p.rapidapi.com/v1/recipe',
-            params: {query: input},
-            headers: {
-              'X-RapidAPI-Key': '08e5532811msh88cf7d8031a47a0p1e1db8jsn0d4bf4f97a9d',
-              'X-RapidAPI-Host': 'recipe-by-api-ninjas.p.rapidapi.com'
-            }
-        };
+    //     const recipe = {
+    //         method: 'GET',
+    //         url: 'https://recipe-by-api-ninjas.p.rapidapi.com/v1/recipe',
+    //         params: {query: input},
+    //         headers: {
+    //           'X-RapidAPI-Key': '08e5532811msh88cf7d8031a47a0p1e1db8jsn0d4bf4f97a9d',
+    //           'X-RapidAPI-Host': 'recipe-by-api-ninjas.p.rapidapi.com'
+    //         }
+    //     };
         
-        axios.request(recipe).then(function (response) {
-            setRecipe(response.data);
-        }).catch(function (error) {
-            console.error(error);
-        });
+    //     axios.request(recipe).then(function (response) {
+    //         setRecipe(response.data);
+    //     }).catch(function (error) {
+    //         console.error(error);
+    //     });
 
        
+    // }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();  
+        setterRecipes(input, setRecipe)
     }
 
     return (
     <div className="nutrition">
         <form onSubmit={handleSubmit}>
-            <label>Food:
-            <input type="text" value={input} onChange={(e) => setInput(e.target.value)}/>
-            </label>
-            <input type="submit" value="View Nutrition and Recipe"/>
+            <SearchBar type="text" value={input} onChange={(e) => setInput(e.target.value)}/>
         </form>
         {recipe &&
             <RecipeList>
